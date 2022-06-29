757685db7eff9c9bcd4caf63ae6638c824493c1c
Freddy Jose <freddywasjose@gmail.com>
2022-06-29 23:34:08 +0530

    added a method to get sql commands to persist data

--- BhavCopyParser/DataBase/Sqlite/SQLiteDBCommandGenerator.cs
+++ BhavCopyParser/DataBase/Sqlite/SQLiteDBCommandGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using BhavCopyParser.BhavCopy.EquityBhavCopy;
+using BhavCopyParser.DataModels.EquityData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +15,28 @@ namespace BhavCopyParser.DataBase.Sqlite
             List<string> commands = new List<string>();
             foreach (string tableName in tableNames)
             {
-                string commandText = @"CREATE TABLE IF NOT EXISTS " + tableName + " (DateInt INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL, NumberOfTrades REAL, VWAP REAL)";
+                string commandText = @"CREATE TABLE IF NOT EXISTS '" + tableName + "' (DateInt INTEGER PRIMARY KEY, Date TEXT, Open REAL, High REAL, Low REAL, Close REAL, Volume REAL, NumberOfTrades REAL, VWAP REAL)";
                 commands.Add(commandText);
             }
             return commands;
         }
 
+        public List<string> GetInsertCommands(EquityBhavCopyParserOutput inputData)
+        {
+            List<string> commands = new List<string>();
+            foreach (var input in inputData.data)
+            {
+                string stockSymbol = input.Key;
+                List<OHLC> ohlc = input.Value;
+                string statement = "INSERT INTO '" + stockSymbol + "' (DateInt, Date, Open, High, Low, Close, Volume, NumberOfTrades, VWAP) VALUES ";
+                foreach (var data in ohlc)
+                {
+                    string valStr = data.Date + ", " + data.Date + ", " + data.Open + ", " + data.High + ", " + data.Low + ", " + data.Close + ", " + data.Volume + ", " + data.NumberOfTrades + ", " + data.VWAP;
+                    statement = statement + ",(" + valStr + ")";
+                }
+                commands.Add(statement);
+            }
+            return commands;
+        }
     }
 }
