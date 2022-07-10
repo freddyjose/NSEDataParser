86c46423a95ef4b138c20eb8a5c443f8a00f56c2
Freddy Jose <freddywasjose@gmail.com>
2022-07-10 17:42:30 +0530

    added the ability to obtain nifty composition

--- BhavCopyParser/Reports/NiftyComposition/NiftyCompositionParser.cs
+++ BhavCopyParser/Reports/NiftyComposition/NiftyCompositionParser.cs
@@ -1,7 +1,9 @@
 ﻿using BhavCopyParser.BhavCopy.FileSystem;
 using BhavCopyParser.DataModels.NiftyComposition;
+using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +12,47 @@ namespace BhavCopyParser.Reports.NiftyComposition
 {
     public class NiftyCompositionParser
     {
-        public NiftyCompositionParser(IDirectoryOps @object)
-        {
-            Object = @object;
-        }
+        private IDirectoryOps directoryOps;
 
-        public IDirectoryOps Object { get; }
+        public NiftyCompositionParser(IDirectoryOps DirectoryOps)
+        {
+            directoryOps = DirectoryOps;
+        }
 
         public List<NiftyComponents> GetNiftyComponents(string startDate, string endDate, string niftyDataFolder)
         {
-            return null;
+            List<NiftyComponents> retVal = new List<NiftyComponents>();
+            List<string> fileNames = directoryOps.GetListOfFiles(niftyDataFolder, startDate, endDate);
+            foreach (string fileName in fileNames)
+            {
+                string fullFileName = Path.Combine(niftyDataFolder, fileName);
+                using (ZipArchive zipFile = ZipFile.Open(fullFileName, ZipArchiveMode.Read))
+                {
+                    IReadOnlyCollection<ZipArchiveEntry> zippedFiles = zipFile.Entries;
+                    ZipArchiveEntry niftyCompositionFile = zippedFiles.First(x => x.Name == "nifty50_mcwb.csv");
+                    Stream niftyCompositionStream = niftyCompositionFile.Open();
+                    using (TextFieldParser csvParser = new TextFieldParser(niftyCompositionStream))
+                    {
+                        int currentPos = 0;
+                        csvParser.TextFieldType = FieldType.Delimited;
+                        csvParser.SetDelimiters(",");
+                        string[] firstRow = csvParser.ReadFields();
+                        string[] secondRow = csvParser.ReadFields();
+                        string[] headerRow = csvParser.ReadFields();
+                        NiftyComponents niftyData = new NiftyComponents();
+                        for(int i = 0; i < 50; i++)
+                        {
+                            string[] row = csvParser.ReadFields();
+                            string symbol = row[1];
+                            string weightageString = row[6];
+                            float weightage = float.Parse(weightageString);
+                            niftyData.components.Add(symbol, weightage);
+                        }
+                        retVal.Add(niftyData);
+                    }
+                }
+            }
+            return retVal;
         }
     }
 }
