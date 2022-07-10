59dff903cadd8e6df8b369e7b695459be9aea0bb
Freddy Jose <freddywasjose@gmail.com>
2022-07-10 13:32:45 +0530

    skeleton of the parser and the test method

---
+++ BhavCopyParser/DataModels/NiftyComposition/NiftyComponents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhavCopyParser.DataModels.NiftyComposition
+{
+    public class NiftyComponents
+    {
+        public Dictionary<string, float> components = new Dictionary<string, float>();
+    }
+}
---
+++ BhavCopyParser/Reports/NiftyComposition/NiftyCompositionParser.cs
@@ -0,0 +1,25 @@
+﻿using BhavCopyParser.BhavCopy.FileSystem;
+using BhavCopyParser.DataModels.NiftyComposition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhavCopyParser.Reports.NiftyComposition
+{
+    public class NiftyCompositionParser
+    {
+        public NiftyCompositionParser(IDirectoryOps @object)
+        {
+            Object = @object;
+        }
+
+        public IDirectoryOps Object { get; }
+
+        public List<NiftyComponents> GetNiftyComponents(string startDate, string endDate, string niftyDataFolder)
+        {
+            return null;
+        }
+    }
+}
